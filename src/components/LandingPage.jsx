31e08660b0d4724f927a40c0729f939f707d9b8b
jonas <jonas.masharqa@gmail.com>
2020-02-11 16:00:07 +0100

    Adds axios get request in LandingPage.jsx and semantic ui react.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 
 class LandingPage extends Component {
-  render () {
+  state = {
+    users: [],
+    errorMessage: null
+  };
+
+  getUsers() {
+    axios.get(`https://reqres.in/api/users?page=1`).then(response => {
+      if (response.status === 200) {
+        this.setState({
+          users: response.data.data
+        });
+      } else {
+        this.setState({
+          errorMessage: "Couldn't load users.."
+        });
+      }
+    });
+  }
+
+  render() {
+    let users;
+
     return <></>;
   }
 }

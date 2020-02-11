a66e5e6430344eda93787bbe852866a6890af566
jonas <jonas.masharqa@gmail.com>
2020-02-11 15:48:48 +0100

    Adds integration and fixture file for viewing fetched users.

---
+++ cypress/integration/userCanViewFetchedUsersOnVisitingLandingPage.spec.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+describe('', () => {
+  it('successfully', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: 'https://reqres.in/api/users?page=1',
+      response: 'fixture:users.json'
+    });
+    cy.visit('http://localhost:3000');
+
+    cy.get('#users').within(() => {
+      cy.get('#user-1').within(() => {
+        cy.get('#first_name-1');
+        cy.get('#last_name-1');
+        cy.get('#email-1');
+      });
+    });
+  });
+});
